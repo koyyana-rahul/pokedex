09dc4940f4268dd495dc30b957c9779e16b972af
unknown <koyyanarahul4002@gmail.com>
2024-07-09 19:38:11 +0530

    initial setup done

---
+++ src/components/Pokedex/Pokedex.jsx
@@ -0,0 +1,15 @@
+//css imports 
+import Search from '../Search/Search';
+import './Pokedex.css';
+
+function Pokedex() {
+     return (
+          <div>
+               <h1>Pokedex</h1>
+               <Search />
+          </div>
+     )
+}
+
+export default Pokedex;
+
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,11 @@
+//css imports
+
+import './Search.css';
+
+function Search() {
+     return (
+          <input type="text" placeholder="which pokemon you're looking for?"/>
+     )
+}
+
+export default Search;
